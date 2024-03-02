a22ba54ec0bed5a1f3db79b30b3cf0a44ede68bf
rich <rlucas11@yahoo.com>
2024-03-02 14:46:56 -0500

    Fixed parcel command and changes arguments

--- R/utils.R
+++ R/utils.R
@@ -636,63 +636,80 @@ modelCode <- function(pcOutput) {
 #' @param data Dataframe with just the items to be parcelled.
 #' @param items Number of items for the scale.
 #' @param waves Number of waves.
-#' @param pattern Vector specifying the number of parcels and the number of
-#'   items per parcel. For example, to get three parcels with 3 items for the
-#'   first parcel, 3 items for the second, and 4 items for the third, specify
-#'   `pattern = c(3, 3, 4)`
+#' @param parcels Number of parcels to create. 
 #'
 #' @export
-parcel <- function(data,items,waves,pattern) {
+parcel <- function(data,items,waves,parcels) {
 
-#Create Temporary Matrix
-loadingSum <- matrix(0,items,1)
-
-#Get Average Loading
-for (i in 1:waves) {
-  start=1+(i-1)*items
-  finish=i*items
-  loadingSum <- loadingSum+stats::factanal(na.omit(data[,start:finish]),
-                                           1)$loadings[,1]
-}
-loadingAvg <- loadingSum/waves
-
-#Rank Items by Loading
-itemList <- data.frame(cbind(loadingAvg,matrix(1:items,items,1)))
-itemList <- itemList[order(itemList$X1),]
-
-#Create Pattern Matrix for Creating Parcels
-temp <- matrix(nrow=max(pattern),ncol=length(pattern))
-for (i in 1:max(pattern)) {
-  if (gtools::odd(i)) {
-    temp[i,] <- ((1+((i-1)*(length(pattern)))):(3+((i-1)*(length(pattern)))))
-  }
-  else {
-    temp[i,] <- ((length(pattern)*i):(length(pattern)*i-2))
-  }
-}
-
-#Create Parcels
-for (i in 1:waves){
-  if (gtools::odd(max(pattern))) newPattern <- pattern else {
-    newPattern <- pattern
-    newPattern[1] <- pattern[3]
-    newPattern[2] <- pattern[2]
-    newPattern[3] <- pattern[1]
-  }
-  for (j in 1:length(newPattern)) {
-    parcel=0
-    maxrows <- newPattern[j]  
-    for (k in 1:maxrows) {
-      parcel <- parcel+data[,((i-1)*items+itemList[temp[k,j],2])]
+    split_integer <- function(number, parts) {
+        quotient <- floor(number / parts)
+        remainder <- number %% parts
+        
+        result <- rep(quotient, times = parts)
+        
+        if (remainder > 0) {
+            result[1:remainder] <- result[1:remainder] + 1
+        }
+        
+        return(result)
     }
-    indexw <- paste("W",i,sep="")
-    indexp <- paste("P",j,sep="")
-    index <- paste(indexw,indexp,sep="")
-    data[,paste("parcel",index,sep="")] <- parcel/newPattern[j]
-  }
-}
 
-return(data[,(items*waves+1):(items*waves+length(pattern)*waves)])
+    pattern <- split_integer(items, parcels)
+    
+    ## Create Temporary Matrix
+    loadingSum <- matrix(0,items,1)
+
+    ## Get Average Loading
+    for (i in 1:waves) {
+        start=1+(i-1)*items
+        finish=i*items
+        loadingSum <- loadingSum+stats::factanal(na.omit(data[,start:finish]),
+                                                 1)$loadings[,1]
+    }
+    loadingAvg <- loadingSum/waves
+
+    ## Rank Items by Loading
+    itemList <- data.frame(cbind(loadingAvg,matrix(1:items,items,1)))
+    itemList <- itemList[order(itemList$X1),]
+
+    create_matrix <- function(rows, cols) {
+        ## Initialize an empty matrix
+        mat <- matrix(0, nrow = rows, ncol = cols)
+        
+        ## Fill the matrix with sequential values
+        counter <- 1
+        for (i in 1:rows) {
+            for (j in 1:cols) {
+                mat[i, j] <- counter
+                counter <- counter + 1
+            }
+        }
+        
+        ## Reverse the even rows in the matrix
+        mat[seq(2, rows, by = 2), ] <- mat[seq(2, rows, by = 2), ncol(mat):1]
+        
+        return(mat)
+    }
+
+    temp <- create_matrix(max(pattern), length(pattern))
+    
+    ##Create Parcels
+    for (i in 1:waves){
+        for (j in 1:parcels) {
+            ##print(j)
+            parcel.idx <- temp[, j][temp[, j] %in% 1:items] + ((i - 1) * items)
+            ##print(parcel.idx)
+            if (length(parcel.idx) > 1) {
+                parcel <- apply(data[, parcel.idx], 1, FUN = mean, na.rm = TRUE)
+            } else {
+                parcel <- data[, parcel.idx]
+            }
+            index <- paste0("W", i, "P", j)
+            data[,paste0("parcel",index)] <- parcel
+        }
+    }
+
+    return(data)
 
 }
 
